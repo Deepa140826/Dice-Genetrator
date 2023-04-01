3ba30deddec3998f1638961ee6a35b7a57586328
Deepa Elangovan <109533865+Deepa140826@users.noreply.github.com>
2023-04-01 12:11:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var randomNumber1=Math.random()*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+
+
+var randomImage1="dice" + randomNumber1 + ".png";
+
+var randomImagesource1="C:/Users/student.DESKTOP-CHNCEBS/Documents/workshtml/DOM/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/"+randomImage1;
+
+var Image1=document.querySelectorAll("img")[0];
+
+Image1.setAttribute("src",randomImagesource1);
+
+
+var randomNumber2=Math.random()*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+
+var randomImage2="dice" + randomNumber2 +".png";
+
+var randomImagesource2="C:/Users/student.DESKTOP-CHNCEBS/Documents/workshtml/DOM/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/"+randomImage2;
+
+var Image2=document.querySelectorAll("img")[1];
+
+Image2.setAttribute("src",randomImagesource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="✌️Player1 wins!!";
+}
+
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="✌️Player2 wins!!";
+}
+
+else{
+    document.querySelector("h1").innerHTML="!😱!";
+}
